60eeca96676b2637762aec81285d3686644bb40a
Naval Shah <navalmaulikshah@gmail.com>
2024-05-18 14:00:31 -0400

    left and right functionality

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import os
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
+#Made it once
+#os.mkdir('Output Images')
+
+
 cap = cv2.VideoCapture(0)
 
 
@@ -17,7 +21,6 @@ with mp_hands.Hands(min_detection_confidence = 0.8, min_tracking_confidence=0.5)
         frame = cv2.flip(frame, 1)
 
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
 
         image.flags.writeable = False
         results = hands.process(image)
@@ -30,19 +33,92 @@ with mp_hands.Hands(min_detection_confidence = 0.8, min_tracking_confidence=0.5)
             for num, hand in enumerate(results.multi_hand_landmarks):
                 mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS)
 
-        cv2.imwrite(
-            os.path.join('Output Images', '{}.jpg'.format(uuid.uuid1())), image
-        )
+        #I don't want to fill my computer with images of my face, so I'll only do this when I have to process the image
+        #cv2.imwrite(os.path.join('Output Images', '{}.jpg'.format(uuid.uuid1())), image)
 
+        
         cv2.imshow('Hand Tracking', image)
 
-        if cv2.waitKey(10) & 0xFF == ('z'):
+        if cv2.waitKey(10) & 0xFF == ('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
+mp_hands.HAND_CONNECTIONS
+
+#So now I can track the image with a flipped image. Now I just need to add the angles between the fingers and maybe some
+#if statements to generate a passcode things haha. Hmm. I can use the mediapipe landmarks to do that. Let's do it.
+#Let's do left and right hands first, that might work
+
+mp_hands.HandLandmark.WRIST
+results.multi_hand_landmarks[1]
+results.multi_handedness[0].classification[0].index == num
+round(results.multi_handedness[0].classification[0].score, 2)
+
+
+def get_label(index, hand, results):
+    output = None
+    for idx, classification in enumerate(results.multi_handedness):
+        if classification.classification[0].index == index:
+            
+            # Process results
+            label = classification.classification[0].label
+            score = classification.classification[0].score
+            text = '{} {}'.format(label, round(score, 2))
+            
+            # Extract Coordinates
+            coords = tuple(np.multiply(
+                np.array((hand.landmark[mp_hands.HandLandmark.WRIST].x, hand.landmark[mp_hands.HandLandmark.WRIST].y)),
+            [640,480]).astype(int))
+            
+            output = text, coords
+            
+    return output
+
+
+get_label(num, hand, results)
+
+cap = cv2.VideoCapture(0)
+
+with mp_hands.Hands(min_detection_confidence = 0.8, min_tracking_confidence = 0.5):
+     while cap.isOpened():
+        ret, frame = cap.read()
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        
+        # Flip on horizontal
+        image = cv2.flip(image, 1)
+        
+        # Set flag
+        image.flags.writeable = False
+        
+        # Detections
+        results = hands.process(image)
+        
+        # Set flag to true
+        image.flags.writeable = True
+        
+        # RGB 2 BGR
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # Detections
+        print(results)
+
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS, 
+                                        mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                                        mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
+                                         )
+                
+                if get_label(num, hand, results):
+                    text, coord = get_label(num, hand, results)
+                    cv2.putText(image, text, coord, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        #cv2.imwrite(os.path.join('Output Images', '{}.jpg'.format(uuid.uuid1())), image)
+        cv2.imshow('Hand Tracking', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
 cap.release()
 cv2.destroyAllWindows()
 
-mp_hands.HAND_CONNECTIONS
-
-#So now I can track the image with a flipped image. Now I just need to add the angles between the fingers and maybe some
-#if statements to generate a passcode things haha. Hmm. I can use the mediapipe landmarks to do that. Let's do it.
